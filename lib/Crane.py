144d642d46ad2190a8a20fa2c4299c958f5db782
antonkluev <dev@antonkluev.com>
2017-11-16 17:29:03 +0100

    crane beautified

--- lib/Crane.py
+++ lib/Crane.py
@@ -35,39 +35,40 @@ class Crane:
 		# FIRST
 		hinge0 = Hinge()
 		hinge0.my_constructor(
-			self.base_node, .1, .01, 
-			avango.gua.make_identity_mat(), 
-			rotAxi0, 
-			[-180, 180])
+			PARENT_NODE = self.base_node, 
+			DIAMETER = .1, 
+			HEIGHT = .01, 
+			ROT_OFFSET_MAT = avango.gua.make_identity_mat(), 
+			ROT_AXIS = rotAxi0, 
+			SF_ROT_INPUT_MAT = [-180, 180])
 		hinge0.sf_rot_value.connect_from(self.input.sf_rot_input0)
-		arm0 = Arm(hinge0.matrix, .01, .1, avango.gua.make_identity_mat())
+		arm0 = Arm(hinge0.matrix, .01, .1)
 		# SECOND
-		rotMat1 = avango.gua.make_identity_mat() * \
-			avango.gua.make_trans_mat(.0, arm0.length, .0)
 		hinge1 = Hinge()
 		hinge1.my_constructor(
-			hinge0.matrix, .02, .01, 
-			rotMat1, rotAxi1,
-			[0, 90])
+			PARENT_NODE = hinge0.matrix, 
+			DIAMETER = .02, 
+			HEIGHT = .01, 
+			ROT_OFFSET_MAT = avango.gua.make_trans_mat(.0, arm0.length, .0), 
+			ROT_AXIS = rotAxi1,
+			SF_ROT_INPUT_MAT = [0, 90])
 		hinge1.sf_rot_value.connect_from(self.input.sf_rot_input1)
-		arm1 = Arm(hinge1.matrix, .005, .08, rotMat1)
+		arm1 = Arm(hinge1.matrix, .005, .08)
 		# THIRD
-		rotMat2 = avango.gua.make_identity_mat() * \
-			avango.gua.make_trans_mat(.0, arm1.length, .0)
 		hinge2 = Hinge()
 		hinge2.my_constructor(
-			hinge1.matrix, .02, .01, 
-			rotMat2, rotAxi1,
-			[-90, 90])
+			PARENT_NODE = hinge1.matrix, 
+			DIAMETER = .02, 
+			HEIGHT = .01, 
+			ROT_OFFSET_MAT = avango.gua.make_trans_mat(.0, arm1.length, .0), 
+			ROT_AXIS = rotAxi1,
+			SF_ROT_INPUT_MAT = [-90, 90])
 		hinge2.sf_rot_value.connect_from(self.input.sf_rot_input2)
-		arm2 = Arm(hinge2.matrix, .005, .08, rotMat2)
+		arm2 = Arm(hinge2.matrix, .005, .08)
 		# FOURTH
 		hook = Hook()
-		hook.rot_mat = avango.gua.make_identity_mat() * \
-			avango.gua.make_trans_mat(.0, arm2.length, .0)
+		hook.rot_mat = avango.gua.make_trans_mat(.0, arm2.length, .0)
 		hook.my_constructor(hinge2.matrix, .02, TARGET_LIST)
 		hinge0.hook = hook
 		hinge1.hook = hook
 		hinge2.hook = hook
-					 
-		
